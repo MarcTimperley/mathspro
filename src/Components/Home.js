596c1f9f995e51f97a12ea3a0b10615536f60392
MarcTimperley <marctimperley@yahoo.co.uk>
2020-03-25 15:20:09 +0000

    new: logo

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,18 +1,18 @@
 import React from 'react'
 import {
     Link
-  } from "react-router-dom"
+} from "react-router-dom"
 import '../App.css'
 
 const Home = () => {
     return (
         <div className="home">
-        <Link className="link" to={{pathname:"/quiz/+"}}><button className="button button-accent button-quiz button-square" id="startQuiz">+</button></Link>
-    <Link className="link" to={{pathname:"/quiz/-"}}><button className="button button-accent button-quiz button-square" id="startQuiz">{`\u2212`}</button></Link>
-        <Link className="link" to={{pathname:"/quiz/x"}}><button className="button button-accent button-quiz button-square" id="startQuiz">{`\u00D7`}</button></Link>
-        <Link className="link" to={{pathname:"/quiz/÷"}}><button className="button button-accent button-quiz button-square" id="startQuiz">{`\u00F7`}</button></Link>
-        <br />
-        <Link className="link" to="/results"><button className="button button-accent button-results button-square" id="results">Results</button></Link>
+            <Link className="link" to={{ pathname: "/quiz/+" }}><button className="button button-accent button-quiz button-square" id="startQuiz">+</button></Link>
+            <Link className="link" to={{ pathname: "/quiz/-" }}><button className="button button-accent button-quiz button-square" id="startQuiz">{`\u2212`}</button></Link>
+            <Link className="link" to={{ pathname: "/quiz/x" }}><button className="button button-accent button-quiz button-square" id="startQuiz">{`\u00D7`}</button></Link>
+            <Link className="link" to={{ pathname: "/quiz/÷" }}><button className="button button-accent button-quiz button-square" id="startQuiz">{`\u00F7`}</button></Link>
+            <br />
+            <Link className="link" to="/results"><button className="button button-accent button-results button-square" id="results">Results</button></Link>
         </div>
 
     )
